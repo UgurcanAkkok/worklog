021cfae3a7db7f0e143ef1a5f9691306dda9140d
Uğurcan Akkök <akkokugurcan@gmail.com>
2025-07-05 22:19:58 +0300

    rm todo for simplifying aliases

--- wl.py
+++ wl.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 ## TODO: integrate with kitty to open the issue in the browser easily (?)
 ## TODO: Is the data format good enough? Should we replace it or is it enough ?
-## TODO: Simplify aliases? e.g. simplify assigning JIRA-000000 for misc stuff, JIRA-000000 for dailies etc.
 import os
 import re
 import argparse
